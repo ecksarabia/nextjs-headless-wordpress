2bdb3df7950383dde55f5ff07af7c34a8d4858e1
ecksarabia <ecksarabia@gmail.com>
2020-04-26 10:47:42 -0500

    Initial commit

---
+++ pages/posts.js
@@ -0,0 +1,38 @@
+import fetch from "isomorphic-unfetch";
+import htmlToReact from "html-to-react";
+import Navigation from "../components/Navigation";
+
+const Posts = ({ posts }) => (
+  <>
+    <Navigation />
+    <h1>Our Posts Page!</h1>
+    {posts.length > 0 &&
+      posts.map((post) => {
+        return (
+          <div>
+            <h2>{`${post.id} ${post.title.rendered}`}</h2>
+            <span>{`Autor: ${post.author}`}</span>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: post.content.rendered,
+              }}
+            ></div>
+          </div>
+        );
+      })}
+  </>
+);
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    "https://www.citaconemprendedores.com/wp-json/wp/v2/posts"
+  );
+  const posts = await res.json();
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+export default Posts;
